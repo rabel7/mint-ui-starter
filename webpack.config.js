3ed96a1c2c84079bafce106b27d05609b8a6547b
daive <52926462@qq.com>
2017-05-07 06:11:25 +0800

    添加说明

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,19 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
+  //入口文件配置
   entry: './src/main.js',
+  //文件输出配置
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
+  //配置要使用的loader，loader列表https://webpack.github.io/docs/list-of-loaders.html
   module: {
     rules: [
       {
+        //解析vue文件
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
@@ -18,6 +22,7 @@ module.exports = {
         }
       },
       {
+        //babel-loader可以把es6的文件转换成es5
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
@@ -39,7 +44,9 @@ module.exports = {
       }
     ]
   },
+  //resolve配置用来影响webpack模块解析规则
   resolve: {
+    //vue$为正则,表示所有vue结尾的文件
     alias: {
       'vue$': 'vue/dist/vue'
     }
@@ -55,11 +62,14 @@ if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
+      //The DefinePlugin allows you to create global constants which can be configured at compile time.
+      //比如生产环境的编译就是最小化的，没有包含热部署等
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
       }
     }),
+      // minify with dead-code elimination
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
